d6ff85f4fed19f871bbb209e7101c8d83645098e
gregatgit <gregdd@outlook.com>
2019-10-13 13:47:50 +1000

    add material ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import purple from '@material-ui/core/colors/purple'
+import green from '@material-ui/core/colors/green'
 
 import MainList from './components/MainList'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+  },
+  status: {
+    danger: 'orange',
+  },
+})
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <h1>My Todo Lab</h1>
       <MainList />
-    </div>
+    </ThemeProvider>
   )
 }
 
