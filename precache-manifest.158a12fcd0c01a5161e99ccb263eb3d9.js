309304e0542d685657716e108b7ae49887d38a7e
gregatgit <gregdd@outlook.com>
2019-10-13 16:07:00 +1000

    Updates

---
+++ precache-manifest.158a12fcd0c01a5161e99ccb263eb3d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795301c7d6e4262a775045368110b05f",
+    "url": "/todos-lab/index.html"
+  },
+  {
+    "revision": "dfb3edc1751a2c80bf2c",
+    "url": "/todos-lab/static/js/2.bbbdec74.chunk.js"
+  },
+  {
+    "revision": "04bff8e800820af49c6d",
+    "url": "/todos-lab/static/js/main.44a7cf56.chunk.js"
+  },
+  {
+    "revision": "41d847a9d8598bada085",
+    "url": "/todos-lab/static/js/runtime~main.738f3ace.js"
+  }
+]);
